15f7fc0d0230ebb46300446c68e79c3f9ea7db03
gmzhang <zhanggm@nocode.com>
2020-12-09 21:01:57 +0800

    feat: add week 03

--- Week03/main.go
+++ Week03/main.go
@@ -1,38 +1,15 @@
-学习笔记
+package main
 
-#### 基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够 一个退出，全部注销退出。
-    ﻿
-
-### 说明
-
-1. 开启 HTTP Server，提供 HTTP 服务
-2. 启动失败，自动退出
-3. 如果有收到 ctrl+c 信号时候，会停止 HTTP 服务
-
-### 编译运行
-
-```shell
-
-go run main.go
-
-// 2020/12/09 21:00:23 start http server
-
-
-// ctrl + c
-
-^C2020/12/09 21:00:36 receive close: interrupt
-2020/12/09 21:00:36 stop http server success
-2020/12/09 21:00:36 exit http server
-2020/12/09 21:00:36 server exit
-
-```
-
-### Code
-
-
-```go
-
-// http service
+import (
+	"net/http"
+	"fmt"
+	"log"
+	"context"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 type httpHandler struct {
 }
@@ -78,9 +55,6 @@ func (h *HttpServer) StopSvc(ctx context.Context) error {
 	return nil
 }
 
-
-// 启动流程
-
 func main() {
 	eg, ctx := errgroup.WithContext(context.Background())
 
@@ -113,6 +87,3 @@ func main() {
 
 	log.Println("server exit")
 }
-
-
-```
